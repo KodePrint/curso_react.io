b3647bb62c524fb7638cc8934bf56c7e71f6d055
Kevin Palma <kapalma05@gmail.com>
2021-12-14 15:23:03 -0600

    [Fix and Bug] add bg a menu

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -4,7 +4,7 @@ import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
 
 
-const API = 'https://api.escuelajs.co/api/v1/products';
+const API = 'https://api.escuelajs.co/api/v1/products/';
 
 const ProductList = () => {
 	const products = useGetProducts(API);
